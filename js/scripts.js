0d03f3cd6f66bb0bac1d0d6dc892718bf283ac19
Philipp Dufter <philipp.dufter@gmail.com>
2016-11-10 15:01:16 +0100

    beautify code

--- js/scripts.js
+++ js/scripts.js
@@ -1,34 +1,34 @@
 function sayHello() {
-   alert("Hello World")
+    alert("Hello World")
 };
 
 
-function get_date(){
-        yy = $('.datepicker').pickadate('picker').get('highlight', 'yy'); 
-        mm = $('.datepicker').pickadate('picker').get('highlight', 'mm'); 
-        dd = $('.datepicker').pickadate('picker').get('highlight', 'dd'); 
-        // working hack..
-        // $('.datepicker').close();
-        //$('.datepicker').deactivate();
-        return dd.toString() + mm.toString() + yy.toString();
+function get_date() {
+    yy = $('.datepicker').pickadate('picker').get('highlight', 'yy');
+    mm = $('.datepicker').pickadate('picker').get('highlight', 'mm');
+    dd = $('.datepicker').pickadate('picker').get('highlight', 'dd');
+    // working hack..
+    // $('.datepicker').close();
+    //$('.datepicker').deactivate();
+    return dd.toString() + mm.toString() + yy.toString();
 };
 
 
 
 
 
-function get_text(day, direction, position, used, used_direction){
+function get_text(day, direction, position, used, used_direction) {
     hashtag = '#' + get_date(day) + "_" + direction
 
     positions_text = "Wir treffen uns " + position + ". "
-    if (position == 'mitte'){
+    if (position == 'mitte') {
         positions_text = "Wir treffen uns in der Mitte. "
     }
 
     if (used == 'nein') {
         ticket = "Das Ticket könnt ihr weiterbenutzen. "
     } else {
-        if (used_direction === undefined | used_direction === ""){
+        if (used_direction === undefined | used_direction === "") {
             ticket = "Ich brauch das Ticket weiterhin."
         } else {
             ticket = "Ich brauch das Ticket weiterhin nach " + used_direction + "."
@@ -41,37 +41,36 @@ function get_text(day, direction, position, used, used_direction){
 
 
 
-function update_text(){
-        day = get_date();
-        direction = $('input[name=direction_selection]:checked', '#direction').val(); 
-        position = $('input[name=position_selection]:checked', '#position').val(); 
-        used = $('input[name=used_selection]:checked', '#used').val(); 
-        if (used == 'ja'){
-            $('.display_used_yes_direction').css('display', 'block');
-        } else {
-            $('.display_used_yes_direction').css('display', 'none');
-        };
-        used_direction = $('#used_yes_direction').val();
-        console.log(used_direction);
-        result = get_text(day, direction, position, used, used_direction);
-        $('#results').val(result);
+function update_text() {
+    day = get_date();
+    direction = $('input[name=direction_selection]:checked', '#direction').val();
+    position = $('input[name=position_selection]:checked', '#position').val();
+    used = $('input[name=used_selection]:checked', '#used').val();
+    if (used == 'ja') {
+        $('.display_used_yes_direction').css('display', 'block');
+    } else {
+        $('.display_used_yes_direction').css('display', 'none');
+    };
+    used_direction = $('#used_yes_direction').val();
+    result = get_text(day, direction, position, used, used_direction);
+    $('#results').val(result);
 };
 
-$(document).ready(function(){
+$(document).ready(function() {
     $('.datepicker').pickadate({
-    selectMonths: false, // Creates a dropdown to control month
-    selectYears: 0, // Creates a dropdown of 15 years to control year
-    onSet: function(context) {
-        this.close();
-  }
-});
-    $('.datepicker').pickadate().pickadate('picker').set('select',  new Date());
+        selectMonths: false, // Creates a dropdown to control month
+        selectYears: 0, // Creates a dropdown of 15 years to control year
+        onSet: function(context) {
+            this.close();
+        }
+    });
+    $('.datepicker').pickadate().pickadate('picker').set('select', new Date());
     update_text();
     $('input').on('change', function() {
         update_text();
     });
 
-    $('#used_yes_direction').on('keyup', function () {
+    $('#used_yes_direction').on('keyup', function() {
         update_text();
     });
 

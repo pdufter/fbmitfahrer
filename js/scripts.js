a731826c0dbbf636c459f62de290e38f46d238b7
Philipp Dufter <philipp.dufter@gmail.com>
2016-11-29 21:04:32 +0100

    add smarter time handling

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
+var TIME_ENTERED = false;
+
 function get_date() {
     yy = $('.datepicker').pickadate('picker').get('highlight', 'yy');
     mm = $('.datepicker').pickadate('picker').get('highlight', 'mm');
@@ -13,9 +15,9 @@ function get_time() {
     return time
 };
 
-function get_text(day, direction, position, used, used_direction) {
+function get_text(day, direction, time, position, used, used_direction) {
     hashtag = '#' + get_date(day) + "_" + direction;
-    time = ' ' + get_time() + '. ';
+    time = ' ' + time + '. ';
     positions_text = "Wir treffen uns " + position + ". ";
     if (position == 'mitte') {
         positions_text = "Wir treffen uns in der Mitte. "
@@ -37,6 +39,20 @@ function get_text(day, direction, position, used, used_direction) {
 function update_text() {
     day = get_date();
     direction = $('input[name=direction_selection]:checked', '#direction').val();
+    if(!TIME_ENTERED){
+        dt = new Date($.now());
+        dep_sbg = 15;
+        dep_muc = 55;
+        if(direction == 'muc_sbg'){
+            mm = dep_muc;
+            hh = update_hour(dep_muc);
+        } else {
+            mm = dep_sbg;
+            hh = update_hour(dep_sbg)
+        }
+        $('#time').val(hh + ':' + mm);
+    }
+    time = get_time();
     position = $('input[name=position_selection]:checked', '#position').val();
     used = $('input[name=used_selection]:checked', '#used').val();
     if (used == 'ja') {
@@ -45,13 +61,21 @@ function update_text() {
         $('.display_used_yes_direction').css('display', 'none');
     };
     used_direction = $('#used_yes_direction').val();
-    result = get_text(day, direction, position, used, used_direction);
+    result = get_text(day, direction, time, position, used, used_direction);
     $('#results').val(result);
 };
 
+function update_hour(threshold) {
+    if(dt.getMinutes() > threshold){
+        hh = (dt.getHours() + 1) % 24
+    } else {
+        hh = dt.getHours()
+    }
+    return hh
+}
+
 $(document).ready(function() {
-    dt = new Date($.now());
-    $('#time').val(dt.getHours() + ':' + dt.getMinutes());
+
     $('.datepicker').pickadate({
         selectMonths: false, // Creates a dropdown to control month
         selectYears: 0, // Creates a dropdown of 15 years to control year
@@ -63,6 +87,9 @@ $(document).ready(function() {
     });
     $('.datepicker').pickadate().pickadate('picker').set('select', new Date());
     update_text();
+    $('#time').on('change', function() {
+        TIME_ENTERED = true;
+    });
     $('input').on('change', function() {
         update_text();
     });
